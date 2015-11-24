75840da397180eaed1e15771153ba7e659414d15
Nghicv <nghicv.57@gmail.com>
2015-11-24 09:01:18 +0700

    static pages
    Authenticate with devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
+  devise :database_authenticatable, :registerable,:recoverable, :rememberable,
+    :validatable
   has_many :exams, dependent: :destroy
   has_many :questions, dependent: :destroy
+
+  validates :name, presence: true
+  validates :chatwork_id, presence: true
 end
