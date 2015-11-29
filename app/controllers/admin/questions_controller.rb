2d254df64eb52112a1ba64fe38f1578eab29198b
duyth1993 <duyth1993@gmail.com>
2015-11-29 16:35:35 +0700

    Admin Update Delete Question

--- app/controllers/admin/questions_controller.rb
+++ app/controllers/admin/questions_controller.rb
@@ -1,6 +1,6 @@
 class Admin::QuestionsController < ApplicationController
   load_and_authorize_resource
-  before_action :load_subjects, only: [:new, :create]
+  before_action :load_subjects, except: [:destroy, :index]
 
   def index
     @q = @questions.ransack params[:q]
@@ -21,9 +21,31 @@ class Admin::QuestionsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @question.update_attributes question_params
+      flash[:success] = t "admin.update_success"
+      redirect_to admin_questions_path
+    else
+      flash[:danger] = t "admin.update_fail"
+      render :edit
+    end
+  end
+
+  def destroy
+    if @question.destroy
+      flash[:success] = t "admin.delete_success"
+    else
+      flash[:danger] = t "admin.delete_fail"
+    end
+    redirect_to admin_questions_path
+  end
+
   private
   def question_params
     params.require(:question).permit :subject_id, :content, :question_type,
-      :status, :user_id, answers_attributes: [:content, :correct, :_destroy]
+      :status, :user_id, answers_attributes: [:id, :content, :correct, :_destroy]
   end
 end
