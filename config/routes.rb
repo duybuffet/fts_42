6d0fa4414b41409c4e4c7b20bb65d9607ddbf375
duyth1993 <duyth1993@gmail.com>
2015-12-01 14:56:29 +0700

    User view contributed questions

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   get "help" => "static_pages#help"
   resources :exams
-  resources :questions, only: [:new, :create]
+  resources :questions, except: :show
   root "exams#index"
 
   namespace :admin do
