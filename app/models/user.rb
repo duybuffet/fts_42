711e6a006463e8a120d127e9dfdc600c4887ff4e
duyth1993 <duyth1993@gmail.com>
2015-12-03 15:02:14 +0700

    FB-Oauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,45 @@
 class User < ActiveRecord::Base
+  TEMP_EMAIL_PREFIX = "change@me"
+  TEMP_EMAIL_REGEX = /\Achange@me/
+
   devise :database_authenticatable, :registerable, :recoverable, :rememberable,
-    :validatable
+    :validatable, :omniauthable
   has_many :exams, dependent: :destroy
   has_many :questions, dependent: :destroy
   enum role: [:member, :admin]
   validates :name, presence: true
-  validates :chatwork_id, presence: true
+  validates_format_of :email, without: TEMP_EMAIL_REGEX, on: :update
+
+  class << self
+    def find_for_oauth auth, signed_in_resource = nil
+      identity = Identity.find_for_oauth auth
+
+      user = signed_in_resource ? signed_in_resource : identity.user
+
+      if user.nil?
+        email_is_verified = auth.info.email && (auth.info.verified || auth.info.verified_email)
+        email = auth.info.email if email_is_verified
+        user = User.find_by(email: email) if email
+
+        if user.nil?
+          user = User.new(
+            name: auth.extra.raw_info.name,
+            email: email ? email : "#{TEMP_EMAIL_PREFIX}-#{auth.uid}-#{auth.provider}.com",
+            password: "password"
+          )
+          user.save!
+        end
+      end
+
+      if identity.user != user
+        identity.user = user
+        identity.save!
+      end
+      user
+    end
+  end
+
+  def email_verified?
+    self.email && self.email !~ TEMP_EMAIL_REGEX
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {omniauth_callbacks: "omniauth_callbacks"}
   get "help" => "static_pages#help"
   resources :exams
   resources :questions, except: :show
   root "exams#index"
+  match "/users/:id/finish_signup", to: "users#finish_signup", via: [:get, :patch], as: :finish_signup
 
   namespace :admin do
     root "dashboard#home"
